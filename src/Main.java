287029792a7e1bb5db820ff6ea914e8af0ab9f24
Kirill <kirpah2015@gmail.com>
2024-06-06 20:12:37 +0300

    corrected version

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 class Main {
-    private static final Map<Character, Integer> romanToIntMap = new HashMap<>();
-    private static final Map<Integer, String> intToRomanMap = new HashMap<>();
+    private static final Map<Character, Integer> romanToIntMap = new LinkedHashMap<>();
+    private static final Map<Integer, String> intToRomanMap = new LinkedHashMap<>();
 
     static {
         romanToIntMap.put('I', 1);
@@ -15,19 +15,19 @@ class Main {
         romanToIntMap.put('D', 500);
         romanToIntMap.put('M', 1000);
 
-        intToRomanMap.put(1, "I");
-        intToRomanMap.put(4, "IV");
-        intToRomanMap.put(5, "V");
-        intToRomanMap.put(9, "IX");
-        intToRomanMap.put(10, "X");
-        intToRomanMap.put(40, "XL");
-        intToRomanMap.put(50, "L");
-        intToRomanMap.put(90, "XC");
-        intToRomanMap.put(100, "C");
-        intToRomanMap.put(400, "CD");
-        intToRomanMap.put(500, "D");
-        intToRomanMap.put(900, "CM");
         intToRomanMap.put(1000, "M");
+        intToRomanMap.put(900, "CM");
+        intToRomanMap.put(500, "D");
+        intToRomanMap.put(400, "CD");
+        intToRomanMap.put(100, "C");
+        intToRomanMap.put(90, "XC");
+        intToRomanMap.put(50, "L");
+        intToRomanMap.put(40, "XL");
+        intToRomanMap.put(10, "X");
+        intToRomanMap.put(9, "IX");
+        intToRomanMap.put(5, "V");
+        intToRomanMap.put(4, "IV");
+        intToRomanMap.put(1, "I");
     }
 
     public static void main(String[] args) {
